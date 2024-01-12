7515818b426a901ae58321f4acd2d32ce105e3df
dens0n <156082076+dens0n@users.noreply.github.com>
2024-01-12 21:45:55 +0100

    move files + fix weather title

--- src/script/background.js
+++ src/script/background.js
@@ -1,16 +1,17 @@
 import axios from "axios";
 
 const backgroundBtn = document.getElementById("background-btn");
+let userInputTheme = "rain";
 
 // Api link + key
 const unsplashApiKey = "2GwNzOMDbOc9794UPXcoNjA2hbnT7NXArsgWZbFOLfg";
-const unsplashUrl = `https://api.unsplash.com/photos/random/?client_id=${unsplashApiKey}&orientation=landscape`;
+const unsplashUrl = `https://api.unsplash.com/photos/random/?client_id=${unsplashApiKey}&orientation=landscape&topics=${userInputTheme}`;
 
 async function getNewImage(unsplashUrl) {
   try {
     const response = await axios.get(unsplashUrl);
-    console.log(response.data.urls);
-    const randomImg = response.data.urls.regular
+    console.log(response.data);
+    const randomImg = response.data.urls.regular;
     document.body.style.backgroundImage = `url(${randomImg})`;
   } catch (error) {
     console.error(error);
--- src/script/weather.js
+++ src/script/weather.js
@@ -22,22 +22,21 @@ document
   .addEventListener("click", getUserInput);
 
 //Api länk + nyckel
-let location = "Stockholm";
+let location = "göteborg";
 const apiKey = "10356db26d3e42269ec124314241101";
 const url = `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${location}&days=4&aqi=no&alerts=no&lang=sv`;
 
 async function getWeatherData(url) {
   try {
-    const response = await axios.get(url, location);
-    const currentWeatherData = response.data.current;
-    console.log(response);
+    const response = await axios.get(url);
+    const currentWeatherData = response.data;
+    console.log(response.data);
     const tomorrowsWeatherData = response.data.forecast.forecastday[1].day;
     const dayAfterTomorrowData = response.data.forecast.forecastday[2].day;
     updateWeather(
       currentWeatherData,
       tomorrowsWeatherData,
-      dayAfterTomorrowData,
-      location
+      dayAfterTomorrowData
     );
   } catch (error) {
     console.error(error);
@@ -50,38 +49,38 @@ getWeatherData(url);
 function getUserInput() {
   // Hämta värdet från inputfältet
   let userInput = document.getElementById("userInput").value;
+  location = userInput.toLowerCase();
 
   // Gör första bokstaven stor
-  location =
-    userInput.charAt(0).toUpperCase() + userInput.slice(1).toLowerCase();
+  // location =
+  //   userInput.charAt(0).toUpperCase() + userInput.slice(1).toLowerCase();
 
   //töm input efter användaren klickat på knappen
   document.getElementById("userInput").value = "";
 
   //skicka med den nya länken med valfri location
   getWeatherData(
-    `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${location}&days=4&aqi=no&alerts=no&lang=sv`,
-    location
+    `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${location}&days=4&aqi=no&alerts=no&lang=sv`
   );
 }
 
-function updateWeather(today, tomorrow, dayAfterTomorrow, location) {
+function updateWeather(today, tomorrow, dayAfterTomorrow) {
   //Hämta container till vädret
   const weatherCard = document.querySelector(".weather");
 
   //logga ut vädret till användaren
   weatherCard.innerHTML = `
-    <h2 class="weather-font">${location}s väder</h2>
+    <h2 class="weather-font">${today.location.name}s väder</h2>
     <div class="weather-card">
       <div class="img-container">
-        <img src="${today.condition.icon}" alt="" class="weather-img" />
+        <img src="${today.current.condition.icon}" alt="" class="weather-img" />
       </div>
       <div class="weather-text">
         <h3>${currentDay}</h3>
-        <p class="c2">${today.temp_c}°C</p>
+        <p class="c2">${today.current.temp_c}°C</p>
       </div>
       <div class="c1">
-        <p>${today.condition.text}</p>
+        <p>${today.current.condition.text}</p>
       </div>
     </div>
  
